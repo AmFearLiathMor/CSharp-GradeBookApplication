861bca3392a33d580edd9e3d29ac5177917697cb
Stuart Harvey <stuartharvey0403@aol.com>
2018-08-30 11:49:26 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,30 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            else
+            {
+                var bandLimit = Students.Count/5;
+                var bandQuery = Students.Where(v => v.AverageGrade >= averageGrade);
+                    
+
+                if (bandQuery.Count() <= bandLimit)
+                    return 'A';
+                else if (bandQuery.Count() <= (bandLimit * 2))
+                    return 'B';
+                else if (bandQuery.Count() <= (bandLimit * 3))
+                    return 'C';
+                else if (bandQuery.Count() <= (bandLimit * 4))
+                    return 'D';
+                else
+                    return 'F';
+            }
+        }
     }
+
+
 }
